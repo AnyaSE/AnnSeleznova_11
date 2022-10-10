5bdbc714c316d8638bb92b8c58a061142a65bd0a
AnyaSE <annseleznova@gmail.com>
2022-10-10 17:19:12 +0800

    added mobile adaptive scss

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function changeVisability () {
     document.getElementById("dropdown__menu").classList.toggle("hide");
 }
+
+let block = document.getElementById('subheader__nav');
+let close = document.getElementById('close');
+
+console.log(subheader__nav);
+console.log(close);
+
+close.addEventListener('click', function () {
+    menubar.style.display = 'none';
+});
